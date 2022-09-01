b78ac400c6645931650c24f5d4a6297a2129d0b1
Batel Haiylo <inpo686@gmail.com>
2022-09-01 16:34:40 +0300

    2 ways

--- src/store/reducers/counter-reducer.js
+++ src/store/reducers/counter-reducer.js
@@ -3,9 +3,9 @@ import{UP_ACTION,RESET_ACTION,DOWN_ACTION,UPDATE_COUNT} from '../actions/counter
 export default function CounterReducer(state,action) {
     switch(action.type){
       case UP_ACTION:
-       return state+1
+       return +state+1
       case DOWN_ACTION:
-       return state-1 
+       return +state-1 
       case RESET_ACTION:
        return 0
       case UPDATE_COUNT:
